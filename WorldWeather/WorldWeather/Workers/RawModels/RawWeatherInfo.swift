16dceaae72968e50ed5849053f92535730cb7477
Bassel Ezzeddine <bassel-maro@hotmail.com>
2018-09-16 12:06:18 +0200

    Created WeatherWorker with fetchWeatherInfo and fetchWeatherIcon methods

---
+++ WorldWeather/WorldWeather/Workers/RawModels/RawWeatherInfo.swift
@@ -0,0 +1,35 @@
+//
+//  RawWeatherInfo.swift
+//  WorldWeather
+//
+//  Created by Bassel Ezzeddine on 15/09/2018.
+//  Copyright © 2018 Bassel Ezzeddine. All rights reserved.
+//
+
+import Foundation
+
+struct RawWeatherInfo: Decodable {
+    let dayWeatherList: [DayWeather]
+    
+    struct DayWeather: Decodable {
+        let id: Int
+        let weather_state_name: String
+        let weather_state_abbr: String
+        let wind_direction_compass: String
+        let created: Date
+        let applicable_date: Date
+        let min_temp: Float
+        let max_temp: Float
+        let the_temp: Float
+        let wind_speed: Float
+        let wind_direction: Float
+        let air_pressure: Float
+        let humidity: Int
+        let visibility: Float
+        let predictability: Int
+    }
+    
+    enum CodingKeys: String, CodingKey {
+        case dayWeatherList = "consolidated_weather"
+    }
+}
---
+++ WorldWeather/WorldWeather/Workers/WeatherWorker.swift
@@ -0,0 +1,70 @@
+//
+//  WeatherWorker.swift
+//  WorldWeather
+//
+//  Created by Bassel Ezzeddine on 15/09/2018.
+//  Copyright © 2018 Bassel Ezzeddine. All rights reserved.
+//
+
+import UIKit
+
+class WeatherWorker {
+    
+    // MARK: - Properties
+    let endpoint = "https://www.metaweather.com/api"
+    
+    // MARK: - Methods
+    func fetchWeatherInfo(woeid: String, completionHandler: @escaping(_ rawWeatherInfo: RawWeatherInfo?, _ success: Bool) -> Void) {
+        let urlString = "\(endpoint)/api/location/\(woeid)"
+        guard let url = URL(string: urlString) else {
+            completionHandler(nil, false)
+            return
+        }
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = "GET"
+        request.cachePolicy = .reloadIgnoringLocalCacheData
+        
+        URLSession.shared.dataTask(with: request, completionHandler: {
+            (data, response, error) in
+            if let data = data, error == nil {
+                do {
+                    let decoder = JSONDecoder()
+                    let rawWeatherInfo = try decoder.decode(RawWeatherInfo.self, from: data)
+                    completionHandler(rawWeatherInfo, true)
+                }
+                catch {
+                    completionHandler(nil, false)
+                }
+            }
+            else {
+                completionHandler(nil, false)
+            }
+        }).resume()
+    }
+    
+    func fetchWeatherIcon(iconName: String, completionHandler: @escaping(_ icon: UIImage?) -> Void) {
+        let urlString = "\(endpoint)/static/img/weather/png/64/\(iconName)"
+        guard let url = URL(string: urlString) else {
+            completionHandler(nil)
+            return
+        }
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = "GET"
+        request.cachePolicy = .reloadIgnoringLocalCacheData
+        
+        URLSession.shared.dataTask(with: request, completionHandler: {
+            (data, response, error) in
+            DispatchQueue.main.async {
+                if let data = data, error == nil {
+                    let icon = UIImage(data: data)
+                    completionHandler(icon)
+                }
+                else {
+                    completionHandler(nil)
+                }
+            }
+        }).resume()
+    }
+}
