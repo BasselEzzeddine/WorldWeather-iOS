4805e9eaf4ec6da426c6ca7abeb45b4b97a79e1e
Bassel Ezzeddine <bassel-maro@hotmail.com>
2018-09-16 13:11:24 +0200

    Created and configured WeatherConfigurator

---
+++ WorldWeather/WorldWeather/Scenes/Weather/WeatherConfigurator.swift
@@ -0,0 +1,35 @@
+//
+//  WeatherConfigurator.swift
+//  WorldWeather
+//
+//  Created by Bassel Ezzeddine on 16/09/2018.
+//  Copyright © 2018 Bassel Ezzeddine. All rights reserved.
+//
+
+import Foundation
+
+extension WeatherViewController: WeatherPresenterOut {
+}
+
+extension WeatherInteractor: WeatherViewControllerOut {
+}
+
+extension WeatherPresenter: WeatherInteractorOut {
+}
+
+class WeatherConfigurator {
+    
+    // MARK: - Properties
+    static let sharedInstance = WeatherConfigurator()
+    
+    // MARK: - Methods
+    func configure(viewController: WeatherViewController) {
+        let interactor = WeatherInteractor()
+        viewController.output = interactor
+        
+        let presenter = WeatherPresenter()
+        presenter.output = viewController
+        
+        interactor.output = presenter
+    }
+}
